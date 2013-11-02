3b28a2849668070212a0b1a97b11c73a91fa2256
PrinceOfDispersia <ovzolotarev@gmail.com>
2013-11-02 14:55:47 +0400

    Basic project structure started

---
+++ Framework/ME_Framework.h
@@ -0,0 +1,16 @@
+/*
+ *	Molotok Editor, 2013 (C) PrinceOfDispersia
+ *	ME_Framework.h - basic include file of framework
+ *
+ **/
+
+#ifndef ME_FRAMEWORK_H
+#define ME_FRAMEWORK_H
+
+// Include math library
+//
+#include "MathLib/ME_Math.h"
+
+
+
+#endif
---
+++ Framework/MathLib/ME_Math.h
@@ -0,0 +1,5 @@
+/*
+ *	Molotok Editor, 2013 (C) PrinceOfDispersia
+ *	ME_Math.h - basic include file of math library
+ *
+ **/
